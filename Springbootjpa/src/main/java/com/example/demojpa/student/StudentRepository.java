2f130ee71b786c28c48ad5b9bebed5d9abfcce99
Selvam <Selvam@DESKTOP-IF7J5KN>
2021-05-05 12:52:58 +0530

    spring boot jpa

---
+++ Springbootjpa/src/main/java/com/example/demojpa/student/StudentRepository.java
@@ -0,0 +1,7 @@
+package com.example.demojpa.student;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface StudentRepository extends CrudRepository<Student, Integer>{
+
+}
